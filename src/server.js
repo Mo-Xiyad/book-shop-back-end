bad3a49ecfb2694802f0a777319abb98c7ee12c6
Zee <76219804+Mo-Xiyad@users.noreply.github.com>
2021-11-09 14:59:39 +0100

    project created

---
+++ src/server.js
@@ -0,0 +1,36 @@
+import express from "express";
+import listEndpoints from "express-list-endpoints";
+import mongoose from "mongoose";
+import cors from "cors";
+
+import postsRouter from "./services/posts/index.js";
+
+const server = express();
+const port = process.env.PORT || 3001;
+
+// ******************************** MIDDLEWARES ********************************
+
+server.use(cors());
+server.use(express.json());
+
+// ******************************** ROUTES ********************************
+
+server.use("/posts", postsRouter);
+
+// ******************************** ERROR HANDLERS ********************************
+
+mongoose.connect(process.env.MONGO_CONNECTION);
+
+mongoose.connection.on("connected", () => {
+  console.log("Mongo Connected!");
+
+  server.listen(port, () => {
+    console.table(listEndpoints(server));
+
+    console.log(`Server running on port ${port}`);
+  });
+});
+
+mongoose.connection.on("error", (err) => {
+  console.log(err);
+});
